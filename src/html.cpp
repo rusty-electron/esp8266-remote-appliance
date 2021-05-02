fe53196b71592eb354af57301dabd8746c66ed6e
rusty-electron <pritom.gogoi101@gmail.com>
2021-05-02 15:20:01 +0530

    add cpp files

---
+++ src/html.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include "FS.h"
+
+String SendHTML(uint8_t led1stat){
+  String ptr = "<!DOCTYPE html> <html>\n";
+  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr +="<title>LED Control</title>\n";
+  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
+  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
+  ptr +=".button-on {background-color: #1abc9c;}\n";
+  ptr +=".button-on:active {background-color: #16a085;}\n";
+  ptr +=".button-off {background-color: #34495e;}\n";
+  ptr +=".button-off:active {background-color: #2c3e50;}\n";
+  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
+  ptr +="</style>\n";
+  ptr +="</head>\n";
+  ptr +="<body>\n";
+  ptr +="<h1>ESP8266 Web Server</h1>\n";
+  ptr +="<h3>Using Access Point(AP) Mode</h3>\n";
+  
+   if(led1stat)
+  {ptr +="<p>LED1 Status: ON</p><a class=\"button button-off\" href=\"/led1off\">OFF</a>\n";}
+  else
+  {ptr +="<p>LED1 Status: OFF</p><a class=\"button button-on\" href=\"/led1on\">ON</a>\n";}
+
+  ptr +="</body>\n";
+  ptr +="</html>\n";
+  return ptr;
+}
+
+void checkOpen(File f){
+  if (!f) {
+    Serial.println("[ERROR] file open failed!");
+  }else{
+    Serial.println("[SUCCESS] file opened successfully");
+  }
+}
---
+++ src/main.cpp
@@ -0,0 +1,119 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include "FS.h"
+
+/* Put your SSID & Password */
+const char* ssid = "NodeMCU";  // Enter SSID here
+const char* password = "12345678";  //Enter Password here
+
+/* Put IP Address details */
+IPAddress local_ip(192,168,1,1);
+IPAddress gateway(192,168,1,1);
+IPAddress subnet(255,255,255,0);
+
+ESP8266WebServer server(80);
+
+// global variables
+uint8_t LED1pin = D3;
+uint8_t LED1status;
+String PATH = "/state.txt";
+
+uint8_t LED1_remember = false;
+char buffer[2];
+File f;
+
+// function declarations
+void handle_OnConnect();
+void handle_led1on();
+void handle_led1off();
+void handle_NotFound();
+String SendHTML(uint8_t);
+void checkOpen(File);
+
+void setup() {
+  Serial.begin(115200);
+
+  // setup and load from flash
+  if (SPIFFS.begin()){
+    Serial.println("[INFO] filesystem mounted");
+  }
+
+  if (SPIFFS.exists(PATH)){
+    f = SPIFFS.open(PATH, "r+");
+    checkOpen(f);
+    f.seek(0);
+    Serial.println(f.available());
+
+    String value = f.readStringUntil('\n');
+    value.toCharArray(buffer, 2);
+    LED1status = atoi(buffer);
+
+    Serial.println("[INFO] Found saved state: " + String(atoi(buffer)));
+    Serial.println(LED1status);
+  }else{
+    f = SPIFFS.open(PATH, "w+");
+    checkOpen(f);
+    LED1status = false;
+  }
+
+  LED1_remember = LED1status;
+
+  pinMode(LED1pin, OUTPUT);
+  digitalWrite(LED1pin, LED1status);
+
+  // configure webserver
+  WiFi.softAP(ssid, password);
+  WiFi.softAPConfig(local_ip, gateway, subnet);
+  delay(100);
+  
+  server.on("/", handle_OnConnect);
+  server.on("/led1on", handle_led1on);
+  server.on("/led1off", handle_led1off);
+  server.onNotFound(handle_NotFound);
+  
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+
+  if(LED1status != LED1_remember){
+    Serial.println("[INFO] new value written to eeprom");
+    char write_value = (LED1status)? '1':'0';
+    Serial.println(write_value);
+    f.close();
+    f = SPIFFS.open(PATH, "w+");
+    f.println(write_value);
+    f.flush();
+
+    LED1_remember = LED1status;
+  }
+}
+
+void handle_OnConnect() {
+  String saved_state = (LED1status)? "ON":"OFF";
+  digitalWrite(LED1pin, LED1status);
+  
+  Serial.println("GPIO7 Saved State: " + saved_state);
+  server.send(200, "text/html", SendHTML(LED1status)); 
+}
+
+void handle_led1on() {
+  LED1status = 1;
+  digitalWrite(LED1pin, HIGH);
+  Serial.println("GPIO7 Status: ON");
+  server.send(200, "text/html", SendHTML(true)); 
+}
+
+void handle_led1off() {
+  LED1status = 0;
+  digitalWrite(LED1pin, LOW);
+  Serial.println("GPIO7 Status: OFF");
+  server.send(200, "text/html", SendHTML(false)); 
+}
+
+void handle_NotFound(){
+  server.send(404, "text/plain", "Not found");
+}
